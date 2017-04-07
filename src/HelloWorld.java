189898259db76d779724b0aeef66fec078742a51
uacspang <cspang@ualberta.ca>
2017-04-06 21:32:57 -0600

    Add java program HelloWorld for testing.

---
+++ src/HelloWorld.java
@@ -0,0 +1,25 @@
+/**
+ * Class to test Travis-CI, ANT and MAVEN.
+ */
+
+/**
+ * @author Candy
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * Constructor.
+	 */
+	public HelloWorld() {
+		// Nothing to add.
+	}
+
+	/**
+	 * @param args No argument.
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
